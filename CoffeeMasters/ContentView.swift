7ae02f56ac92d74203a8606b95e184d98dcc184c
Deanna Dunne <deanna.dunne@gmail.com>
2023-12-03 20:52:42 -0500

    menu and offers page

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -10,7 +10,28 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        EmptyView()
+        TabView {
+            MenuPage()
+                .tabItem {
+                    Image(systemName: "cup.and.saucer")
+                    Text("Menu")
+                }
+            OffersPage()
+                .tabItem {
+                    Image(systemName: "tag")
+                    Text("Offers")
+                }
+            OrdersPage()
+                .tabItem {
+                    Image(systemName: "cart")
+                    Text("Order")
+                }
+            InfoPage()
+                .tabItem {
+                    Image(systemName: "info.circle")
+                    Text("Info")
+                }
+        }
     }
 }
 
